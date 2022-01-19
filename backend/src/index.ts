a45f3ab7c0a6f11c2fb49317af5b6e29955d80d4
Pavel Shukhman <pavel@reliza.io>
2022-01-19 11:23:32 -0500

    chore: abstract findbom in a separate function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ type GqlUrl = {
     url: string
 }
 
-type Bom = {
+type BomRecord = {
   uuid: string,
   created_date: Date,
   last_updated_date: Date,
@@ -17,7 +17,7 @@ type Bom = {
 type BomInput = {
   bomInput: {
     meta: string,
-    bom: Object,
+    bom: any,
     tags: Object
   }
 }
@@ -84,45 +84,57 @@ const resolvers = {
     hello: () => 'world',
     allBoms: async () => {
       let queryRes = await utils.runQuery('select * from rebom.boms', []) 
-      let boms = queryRes.rows as Bom[]
+      let boms = queryRes.rows as BomRecord[]
       return boms
     },
     findBom: async (parent: any, bomSearch : BomSearch) => {
-      let searchObject = {
-        queryText: `select * from rebom.boms where 1 = 1`,
-        queryParams: [],
-        paramId: 1
-      }
-
-      if (bomSearch.bomSearch.serialNumber) {
-        if (!bomSearch.bomSearch.serialNumber.startsWith('urn')) {
-          bomSearch.bomSearch.serialNumber = 'urn:uuid:' + bomSearch.bomSearch.serialNumber
-        }
-        updateSearchObj(searchObject, `bom->>'serialNumber'`, bomSearch.bomSearch.serialNumber)
-      }
-
-      if (bomSearch.bomSearch.version) updateSearchObj(searchObject, `bom->>'version'`, bomSearch.bomSearch.version)
-
-      if (bomSearch.bomSearch.componentVersion) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'version'`, 
-          bomSearch.bomSearch.componentVersion)
-      
-      if (bomSearch.bomSearch.componentGroup) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'group'`, 
-          bomSearch.bomSearch.componentGroup)
-      
-      if (bomSearch.bomSearch.componentName) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'name'`, 
-          bomSearch.bomSearch.componentName)
-
-      let queryRes = await utils.runQuery(searchObject.queryText, searchObject.queryParams)
-      return queryRes.rows
+      return findBom(bomSearch)
     }
   },
   Mutation: {
-    addBom: async (parent : any, bomInput : BomInput): Promise<Bom> => {
+    addBom: async (parent : any, bomInput : BomInput): Promise<BomRecord> => {
+      // urn must be unique - if same urn is supplied, we update current record
+      // similarly it works for version, component group, component name, component version
+
+      // check if urn is set on bom
+      if (bomInput.bomInput.bom.serialNumber) {
+        // attempt to find existing boms with same urn
+
+      }
       let queryText = 'INSERT INTO rebom.boms (meta, bom, tags) VALUES ($1, $2, $3) RETURNING *'
       let queryRes = await utils.runQuery(queryText, [bomInput.bomInput.meta, bomInput.bomInput.bom, bomInput.bomInput.tags])
       return queryRes.rows[0]
     }
   }
+}
+
+async function findBom (bomSearch: BomSearch): Promise<BomRecord> {
+  let searchObject = {
+    queryText: `select * from rebom.boms where 1 = 1`,
+    queryParams: [],
+    paramId: 1
+  }
+
+  if (bomSearch.bomSearch.serialNumber) {
+    if (!bomSearch.bomSearch.serialNumber.startsWith('urn')) {
+      bomSearch.bomSearch.serialNumber = 'urn:uuid:' + bomSearch.bomSearch.serialNumber
+    }
+    updateSearchObj(searchObject, `bom->>'serialNumber'`, bomSearch.bomSearch.serialNumber)
+  }
+
+  if (bomSearch.bomSearch.version) updateSearchObj(searchObject, `bom->>'version'`, bomSearch.bomSearch.version)
+
+  if (bomSearch.bomSearch.componentVersion) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'version'`, 
+      bomSearch.bomSearch.componentVersion)
+  
+  if (bomSearch.bomSearch.componentGroup) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'group'`, 
+      bomSearch.bomSearch.componentGroup)
+  
+  if (bomSearch.bomSearch.componentName) updateSearchObj(searchObject, `bom->'metadata'->'component'->>'name'`, 
+      bomSearch.bomSearch.componentName)
+
+  let queryRes = await utils.runQuery(searchObject.queryText, searchObject.queryParams)
+  return queryRes.rows
 }
 
 function updateSearchObj(searchObject: SearchObject, queryPath: string, addParam: string) {
